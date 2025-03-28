fc139c3b0c2b68a48a834e70a193374a48041d3a
yassin <yassine.tamam3@gmail.com>
2025-03-28 23:48:03 +0200

    done with whatever the fuck this is

--- src/main/models/breast-cancer.py
+++ src/main/models/breast-cancer.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>MRI model is here</p>"
+    return "<p>breast cancer model is here!!</p>"
--- src/main/models/chest-xray.py
+++ src/main/models/chest-xray.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>MRI model is here</p>"
+    return "<p>chest-xray is here!</p>"
