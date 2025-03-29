2732421e74ca8120c685322e74f1e11679db9b38
Carin Samy <115353460+CarinSamy@users.noreply.github.com>
2025-03-29 19:35:09 +0200

    download_Models(#8)

---
+++ src/main/models/download_models.py
@@ -0,0 +1,26 @@
+import os
+import gdown
+
+File_IDS = {
+    "efficientnetb0_alzheimer.h5": "1woONx90w8AqezObAoq1U8-BqrNR_BI6v",
+    "hybrid_metadata_model.h5": "1I9DLTeuMh25HSBO9qmUURgKD0fqJ8zwp"
+}
+
+OUTPUT_PATH = "models"
+
+def download_models():
+    """Downloads models from Google Drive"""
+    os.makedirs(OUTPUT_PATH, exist_ok=True)
+
+    for file_name, file_id in File_IDS.items():
+        output_path = os.path.join(OUTPUT_PATH, file_name)
+        if not os.path.exists(output_path):
+            print(f"Downloading {file_name}...")
+            url = f"https://drive.google.com/uc?id={file_id}"
+            gdown.download(url, output_path, quiet=False)
+            print(f"Downloaded {file_name} to {output_path}")
+        else:
+            print(f"{file_name} already exists")
+
+if __name__ == "__main__":
+    download_models()
