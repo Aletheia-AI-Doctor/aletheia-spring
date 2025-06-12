2508780c987dc1296d70f532727afb5f6cfc2e06
Fadi Sarwat <fady.sarwat377@gmail.com>
2025-06-12 13:12:58 +0300

    WIP

--- src/main/models/entrypoint_production.sh
+++ src/main/models/entrypoint_production.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-set -e
-
-WEIGHTS_DIR="/app/weights"
-
-mkdir -p "$WEIGHTS_DIR"
-
-NUM_FILES=$(find "$WEIGHTS_DIR" -type f | wc -l)
-
-if [ "$NUM_FILES" -lt 4 ]; then
-    echo "Detected missing or incomplete weights. Downloading..."
-    python download_models.py
-else
-    echo "All weights already exist. Skipping download."
-fi
-
 echo "Starting Flask server..."
 exec gunicorn --bind 0.0.0.0:4000 --workers 4 --threads 2 --timeout 120 --worker-class gevent --access-logfile - --error-logfile -app:app
 
