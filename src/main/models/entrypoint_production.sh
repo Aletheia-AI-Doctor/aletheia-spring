ea6db900ca6e3c0c4e17fb6634de0c5d12146ebd
Fadi Sarwat <fady.sarwat377@gmail.com>
2025-06-12 13:56:56 +0300

    Fix gunicorn command

--- src/main/models/entrypoint_production.sh
+++ src/main/models/entrypoint_production.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "Starting Flask server..."
-exec gunicorn --bind 0.0.0.0:4000 --workers 4 --threads 2 --timeout 120 --worker-class gevent --access-logfile - --error-logfile -app:app
-
+exec gunicorn --bind 0.0.0.0:4000 --workers 4 --threads 2 --timeout 120 --worker-class gevent app:app
