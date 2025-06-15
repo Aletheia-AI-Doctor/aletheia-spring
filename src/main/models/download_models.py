7ff797c5eb0bb86c2993fa0464e3b0e2e8369bbb
Peter-Anton <peto4eskander69@gmail.com>
2025-06-16 01:36:58 +0300

    cahnge mri weight hash

--- src/main/models/download_models.py
+++ src/main/models/download_models.py
@@ -2,7 +2,7 @@ import os
 import gdown
 
 File_IDS = {
-    "efficientnetb0_alzheimer.h5": "1woONx90w8AqezObAoq1U8-BqrNR_BI6v",
+    "efficientnetb0_alzheimer.h5": "1O1YQWeR61hZzvwoIKFBSVCz2TnieU1bV",
     "MRI_Model.keras": "1wWwgYsZDLEo8-u8guLaZFitRrtg2qmX5",
     "last.pt": "12aXVwntxfTvEUcl6WE2Noe83Ia0y7S0E",
     "breast_cancer_detection.keras": "18QuheON6UViSjtaz2GrBRQBbuM3hjcnZ",
