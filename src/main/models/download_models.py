860d19a9d851841346d83f47d51901477612bf2e
Fadi Sarwat <fady.sarwat377@gmail.com>
2025-06-16 02:43:36 +0300

    Fix MRI not working because of .h5 instead of .keras

--- src/main/models/download_models.py
+++ src/main/models/download_models.py
@@ -2,7 +2,7 @@ import os
 import gdown
 
 File_IDS = {
-    "efficientnetb0_alzheimer.h5": "1O1YQWeR61hZzvwoIKFBSVCz2TnieU1bV",
+    "efficientnetb0_alzheimer.keras": "1O1YQWeR61hZzvwoIKFBSVCz2TnieU1bV",
     "last.pt": "12aXVwntxfTvEUcl6WE2Noe83Ia0y7S0E",
     "breast_cancer_detection.keras": "18QuheON6UViSjtaz2GrBRQBbuM3hjcnZ",
 }
