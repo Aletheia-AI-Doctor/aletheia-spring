9d4839c47e06057028f9998397a413225b2f1116
Fadi Sarwat <fadi.sarwat@transformus.co>
2025-07-05 01:18:58 +0300

    WIP

--- src/main/java/dev/aletheia/doctor/dtos/scans/ScanWithUrlDto.java
+++ src/main/java/dev/aletheia/doctor/dtos/scans/ScanWithUrlDto.java
@@ -43,6 +43,6 @@ public class ScanWithUrlDto {
     }
 
     public String getImageResponseUrl() throws GeneralSecurityException {
-        return imageResponse == null || imageResponse.isBlank() ? this.digitalSignService.getSignedUrl("/api/scans/" + imageResponse + "/image") : "";
+        return imageResponse == null || imageResponse.isBlank() ? "" : this.digitalSignService.getSignedUrl("/api/scans/" + imageResponse + "/image");
     }
 }
